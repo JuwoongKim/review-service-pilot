f9c361f29a9cfb8d73399d4e198b4b402248045d
juwoong kim <wwong7247@gmail.com>
2023-10-27 02:14:29 +0900

    리뷰 서비스 파일럿 프로젝트 개발 환경을 구축하다

---
+++ src/main/java/com/example/reviewservicepilot/ReviewServicePilotApplication.java
@@ -0,0 +1,13 @@
+package com.example.reviewservicepilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewServicePilotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewServicePilotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/reviewservicepilot/ReviewServicePilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reviewservicepilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServicePilotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
