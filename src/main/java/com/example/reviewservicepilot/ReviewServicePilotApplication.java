6242001abe82c43dd9898605f39b03142fcda9c5
juwoong kim <wwong7247@gmail.com>
2023-10-30 22:59:20 +0900

    BaseEntity를 생성하고 Auditing 설정을 추가하다

--- src/main/java/com/example/reviewservicepilot/ReviewServicePilotApplication.java
+++ src/main/java/com/example/reviewservicepilot/ReviewServicePilotApplication.java
@@ -2,12 +2,14 @@ package com.example.reviewservicepilot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class ReviewServicePilotApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ReviewServicePilotApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ReviewServicePilotApplication.class, args);
+	}
 
 }
